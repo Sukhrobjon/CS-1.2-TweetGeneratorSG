88935f81452e4209d0f2b0d21be2bb170523b210
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2018-10-23 23:37:44 -0700

    reverse.py done but working on different ways of reversing the word

---
+++ reverse.py
@@ -0,0 +1,18 @@
+
+# version one using loop
+def reverse_words_one(word):
+    start = 0
+    end = len(word)-1
+    while start < end:
+        word[start], word[end] = word[end], word[start]
+        start += 1
+        end -= 1
+    return word
+
+# version two using slicing notation
+def reverse_words_two(word):
+    return word[::-1]
+
+word = 'Sukhrob'
+
+print(reverse_words_two(word))
