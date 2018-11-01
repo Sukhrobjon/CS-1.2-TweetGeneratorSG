b2de5d23fbb7b47b30075d92a25afa28bfd5edc5
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2018-11-01 13:31:25 -0700

    histogram(dictionary) version is completed

--- page-3/histogram.py
+++ page-3/histogram.py
@@ -18,12 +18,57 @@ histogram_dictionary = [{'one': 1, 'blue': 1, 'two': 1, 'fish': 4, 'red': 1}]
 
 '''
 
+'''
+    reads the file and return a list of the the words of that file
+'''
+def read_file():
+    with open('source_text.txt') as file:
+        source_string = file.read().split(' ')
+    return source_string
 
-def histogram():
-    source_text = open('source_text.txt').read().split(' ')
+'''
+    counts the frequency of each words using counter library
+    and returns dictionry data structure as key: word, value: number
+'''
+def histogram(source_text):
     count = collections.Counter(source_text)
     return count
 
+def histogram_dict(source_text):
+    histogram = {}
 
-print(histogram())
+    for word in source_text:
+        if word in histogram:
+            histogram[word] += 1
+        else:
+            histogram[word] = 1
+    return histogram 
 
+
+'''
+    Counts the each word frequency using list of list method
+'''
+def histogram_list(source_text):
+    histogram = []
+    current_word = None
+    for word in source_text:
+        if word == current_word:
+            histogram[-1][1] += 1
+        else:
+            histogram.append([word, 1])
+            current_word = word
+    return histogram
+
+# returns number of unique words in the source text 
+def unique_words(source_text):
+    # return len(set(histogram(source_text)))
+    # return len(histogram(source_text).keys)
+    return len(histogram_dict(source_text))
+# print(histogram())
+
+if __name__ == "__main__":
+    source_text = read_file()
+    # print(histogram(source_text))
+    # print(histogram_list(source_text))
+    print(unique_words(source_text))
+    print(histogram_dict(source_text))
