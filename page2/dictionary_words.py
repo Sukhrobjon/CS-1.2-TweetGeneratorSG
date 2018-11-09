73071bbc4ac861e1de6ef1aa4cfb76f80a9aac5b
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2018-11-08 18:07:37 -0800

    not able to import the module

--- page2/dictionary_words.py
+++ page2/dictionary_words.py
@@ -6,8 +6,8 @@ import timeit
     opens the file and read from it line by line and return to 
     list of the words 
 '''
-def read_file():
-    with open("/usr/share/dict/words") as file:
+def read_file(source):
+    with open(source) as file:
         words = file.read().split('\n')
     return words
     
@@ -16,14 +16,15 @@ def make_random_sentence(num_words):
     '''makes a random sentence out of the read_file() with specific number of words
     according tho user input. And returns to sentence splinting the words by single 
     space'''
+    source = '/usr/share/dict/words'
     new_sentence = []
-    words = read_file()
+    words = read_file(source)
     for _ in range(int(num_words)):
         new_sentence.append(words[random.randint(0, len(words))] + " ")
     return(''.join(new_sentence))
 
 if __name__ == '__main__':
-
+    
     num_words = sys.argv[1]
     print(make_random_sentence(num_words))
     
--- page5/app.py
+++ page5/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from page2.dictionary_words import *
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    
+    return make_random_sentence(10)
 
 if __name__ == '__main__':
     app.run()
