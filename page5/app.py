6ed76feb8cee67739e28c5f48ef61b47c0ff3533
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2018-11-08 18:29:32 -0800

    flask is now working locally

--- page5/app.py
+++ page5/app.py
@@ -1,11 +1,10 @@
 from flask import Flask
-from page2.dictionary_words import *
+from random_sentence import *
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    
     return make_random_sentence(10)
 
 if __name__ == '__main__':
--- page5/random_sentence.py
+++ page5/random_sentence.py
@@ -1,35 +1,35 @@
 import random
-import sys
-import timeit
 
 '''
     opens the file and read from it line by line and return to 
     list of the words 
 '''
+
+
 def read_file(source):
     with open(source) as file:
-        words = file.read().split('\n')
+        words = file.read().split()
     return words
-    
+
 
 def make_random_sentence(num_words):
     '''makes a random sentence out of the read_file() with specific number of words
     according tho user input. And returns to sentence splinting the words by single 
     space'''
-    source = '/usr/share/dict/words'
+    source = 'source.txt'
     new_sentence = []
     words = read_file(source)
     for _ in range(int(num_words)):
         new_sentence.append(words[random.randint(0, len(words))] + " ")
     return(''.join(new_sentence))
 
+
 if __name__ == '__main__':
-    
+
     # num_words = sys.argv[1]
     num_words = 10
     print(make_random_sentence(num_words))
-    
-    
+
 
 '''
 timeit built in banch mark library
