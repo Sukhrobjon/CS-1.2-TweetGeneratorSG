d839f1b9088dae280da88a5fd778d3dea0dc6961
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2018-12-12 15:17:22 -0800

    small change

--- cleanup.py
+++ cleanup.py
@@ -1,20 +1,11 @@
 import re
+
 def read_file(txt):
     with open(txt) as file:
         contents = file.read()
     contents = polishing_text(contents)
 
     return contents
-
-
-# def polishing_text(string):
-#     punc = '''!()-[]{};:"\<>./?@#$%^&*_~1234567890'''
-# #     punc = '''!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~+'''
-#     no_punc = ""
-#     for char in string:
-#         if char not in punc:
-#             no_punc += char
-#     return no_punc
 
 
 def polishing_text(source_text):
